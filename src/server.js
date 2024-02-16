2183a0bec1bff4b4853e954dc3f99bb79f190174
Natan Oliveira <natanoliveirati@gmail.com>
2024-02-15 21:35:06 -0300

    Update server.js

--- src/server.js
+++ src/server.js
@@ -9,29 +9,6 @@ app.use(bodyParser.urlencoded({ "extended": false }))
 // app.listen(3000)
 const PORT_SERVER = 3000
 
-// app.use('/', (req, res, next) => {
-//     const timeout = 90000; // 90 segundos (ajuste conforme necessário)
-//     let isTimeout = false;
-
-//     // Configura um temporizador para acionar um timeout
-//     const timeoutId = setTimeout(() => {
-//         isTimeout = true;
-//         const erro = new Error('Tempo limite de solicitação excedido');
-//         erro.status = 504; // Gateway Timeout
-//         next(erro);
-//     }, timeout);
-
-//     // Chama o próximo middleware (exRouter)
-//     next();
-
-//     // Quando a solicitação é concluída (após exRouter), cancela o temporizador se ainda não tiver ocorrido um timeout
-//     res.on('finish', () => {
-//         if (!isTimeout) {
-//             clearTimeout(timeoutId);
-//         }
-//     });
-// });
-
 // require('./app/controllers/authController')(app)
 // require('./app/controllers/projetcController')(app)
 require('./app/controllers/index')(app)
